31651a5ef46bf02ba2fa183ab20ea6b2e12c95cf
aldoogarcia <67659817+aldoogarcia@users.noreply.github.com>
2024-11-13 22:56:49 +0000

    FEAT: AGREGA SECUENCIA DE MIDDLEWARE

---
+++ app.js
@@ -0,0 +1,32 @@
+// Importando el modulo http
+import http from 'http';
+// Importando Express
+import express from 'express';
+
+// Creando la instancia de express
+// que basicamente es un middleware
+const app = express();
+
+// Registrando el primer middleware
+app.use((req, res, next)=>{
+  console.log("📢 Middleware #1");
+  // Se invoca al siguiente middlware
+  next();
+});
+// Registrando el segundo middleware
+app.use((req, res, next)=>{
+  console.log("📢 Middleware #2");
+  // Se invoca al siguiente middlware
+  next();
+});
+// Asignando el middleware de express
+// al servidor http
+const server = http.createServer(app);
+
+// Definiendo puertos
+const port = 3000;
+const ip = "0.0.0.0"
+// Arrancando el servidor
+server.listen(port, ip, () => {
+  console.log(`🤖 Sirviendo en http://localhost:${port}`);
+});
